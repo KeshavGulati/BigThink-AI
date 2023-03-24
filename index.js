4ac11f45478748c4d484197fdd6059df3367aa63
Keshav <keshavgulati785@gmail.com>
2023-03-23 22:30:09 -0400

    Completed both desktop and mobile versions

---
+++ index.js
@@ -0,0 +1,25 @@
+const hamburgerDiv = document.querySelector(".hamburger-div");
+const navBar = document.querySelector(".nav__nav-items");
+const navLinks = document.querySelectorAll(".nav__nav-items__a");
+
+hamburgerDiv.addEventListener('click', () => {
+    const status = hamburgerDiv.getAttribute("status");
+    if (status === "open") {
+        hamburgerDiv.setAttribute("status", "close");
+        navBar.setAttribute("data-visible", "false");
+        document.querySelector("body").style.overflowY = "hidden";
+    } else {
+        hamburgerDiv.setAttribute("status", "open");
+        navBar.setAttribute("data-visible", "true");
+        document.querySelector("body").style.overflowY = "scroll";
+    }
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        const href = link.getAttribute("href");
+        hamburgerDiv.setAttribute("status", "close");
+        navBar.setAttribute("data-visible", "false");
+        document.getElementById(href).scrollIntoView();
+    });
+});
