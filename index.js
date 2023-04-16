fac0d4138bc91c2112fd4339268d8c3454b191d8
Keshav <keshavgulati785@gmail.com>
2023-04-15 20:06:20 -0400

    Implemented slideshow for mobile view

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const navBar = document.querySelector(".nav__nav-items");
 const navLinks = document.querySelectorAll(".nav__nav-items__a");
 const pyoneersDiv = document.querySelector(".pyoneers-div");
 const edgeDiv = document.querySelector(".edge-div");
+const aiDiv = document.querySelector(".AI-div");
+const dsDiv = document.querySelector(".DS-div");
+const pythonDiv = document.querySelector(".Python-div");
+const pyoneersSubDiv = document.querySelector(".pyoneers-div__sub-div");
 // const pyoneersSubDiv = document.querySelectorAll(".pyoneers-div__sub-div > div")
 
 // Can add animations for borders of pyoneers' subdiv
@@ -58,3 +62,23 @@ navLinks.forEach(link => {
         document.body.style.overflowY = "scroll";
     });
 });
+
+setInterval(() => {
+    // console.log(dsDiv.getAttribute("status"));
+    if (window.innerWidth > 1200) {
+        return;
+    }
+    if (aiDiv.getAttribute("status") == "active") {
+        pyoneersSubDiv.style.transform = "translateX(-33.33334%)";
+        aiDiv.setAttribute("status", "inactive");
+        dsDiv.setAttribute("status", "active");
+    } else if (dsDiv.getAttribute("status") == "active") {
+        pyoneersSubDiv.style.transform = "translateX(-66.66667%)";
+        dsDiv.setAttribute("status", "inactive");
+        pythonDiv.setAttribute("status", "active");
+    } else {
+        pyoneersSubDiv.style.transform = "translateX(0vw)";
+        pythonDiv.setAttribute("status", "inactive");
+        aiDiv.setAttribute("status", "active");
+    }
+}, 2500);
